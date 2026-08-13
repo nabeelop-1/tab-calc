c7040576100b74e3bd8ff132e2f1bb79797fb620
nabeelop-1 <nabeelop789789@gmail.com>
2026-08-13 22:05:42 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "tab-calc-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
